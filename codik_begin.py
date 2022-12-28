e203045b0a6b2268bf31c5ba1d7c6b3e2b78ca2d
IriShamanova <121150644+IriShamanova@users.noreply.github.com>
2022-12-28 15:56:40 +0300

    Add files via upload

---
+++ codik_begin.py
@@ -0,0 +1,14 @@
+person_name = 'Max'
+print(type(person_name))
+
+age = 30
+print(type(age))
+
+period = 3.2
+print(type(period))
+
+is_good = True
+print(type(is_good))
+
+person = None
+print(type(person))
